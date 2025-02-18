19859d1357b4ce64b725acaec095e8d9287bfaa1
deepakchamola <deepak.chamola1546@gmail.com>
2025-02-18 18:42:07 +0530

    added image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import img1 from "/src/assets/img1.png";
 import img2 from "/src/assets/img2.png";
 import img3 from "/src/assets/img3.png";
+import img4 from "/src/assets/img4.png";
 import { motion } from "framer-motion";
 import Cards from "./Cards";
 
